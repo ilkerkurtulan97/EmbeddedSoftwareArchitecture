6bf1567be846f4e6481ed6b2a0091ad51b506223
ilkerkurtulan97 <45030821+ilkerkurtulan97@users.noreply.github.com>
2019-12-26 22:53:56 +0300

    Update interrupts_msp432p401r_gcc.c

--- interrupts_msp432p401r_gcc.c
+++ interrupts_msp432p401r_gcc.c
@@ -1,61 +1,10 @@
-//*****************************************************************************
-//
-// Copyright (C) 2012 - 2016 Texas Instruments Incorporated - http://www.ti.com/
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-//
-//  Redistributions of source code must retain the above copyright
-//  notice, this list of conditions and the following disclaimer.
-//
-//  Redistributions in binary form must reproduce the above copyright
-//  notice, this list of conditions and the following disclaimer in the
-//  documentation and/or other materials provided with the
-//  distribution.
-//
-//  Neither the name of Texas Instruments Incorporated nor the names of
-//  its contributors may be used to endorse or promote products derived
-//  from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// MSP432P401R Interrupt Vector Implementation and startup code for GCC
-//
-//****************************************************************************
-
 #include <stdint.h>
 
-/* Entry point for the application. */
 extern int main(void);
 
-/* External declaration for system initialization function                  */
 extern void SystemInit(void);
 
-/* Forward declaration of the default fault handlers. */
-/* This is the code that gets called when the processor first starts execution */
-/* following a reset event.  Only the absolutely necessary set is performed,   */
-/* after which the application supplied entry() routine is called.  Any fancy  */
-/* actions (such as making decisions based on the reset cause register, and    */
-/* resetting the bits in that register) are left solely in the hands of the    */
-/* application.
- *
- * This is the code that gets called when the processor first starts execution */
-/* following a reset event.  Only the absolutely necessary set is performed,   */
-/* after which the application supplied entry() routine is called.  Any fancy  */
-/* actions (such as making decisions based on the reset cause register, and    */
-/* resetting the bits in that register) are left solely in the hands of the    */
-/* application.                                                                */
+
 extern uint32_t __data_load__;
 extern uint32_t __data_start__;
 extern uint32_t __data_end__;
